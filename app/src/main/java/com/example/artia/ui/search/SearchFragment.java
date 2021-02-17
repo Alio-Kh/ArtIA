2a18ad6d10cbe25f414916204ee2900f18d061c8
Moïse Gui <k.moise.gui@gmail.com>
2021-02-17 20:05:40 +0100

    CameraPreview implemented need to add the permissions prompt cause not done implicitly

--- app/src/main/java/com/example/artia/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/artia/ui/search/SearchFragment.java
@@ -1,21 +1,25 @@
 package com.example.artia.ui.search;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.artia.R;
 
 public class SearchFragment extends Fragment {
 
+    private Camera mCamera;
+    private CameraPreview mPreview;
     private SearchViewModel searchViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -23,14 +27,50 @@ public class SearchFragment extends Fragment {
         searchViewModel =
                 new ViewModelProvider(this).get(SearchViewModel.class);
         View root = inflater.inflate(R.layout.fragment_search, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        searchViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+        mCamera.setDisplayOrientation(90);
+
+        if (mCamera != null) {
+            // Create our Preview view and set it as the content of our activity.
+            mPreview = new CameraPreview(getContext(), mCamera);
+
+            FrameLayout preview = (FrameLayout) root.findViewById(R.id.camera_preview);
+            preview.addView(mPreview);
+        } else {
+            Log.d("Search Fragment", "Camera was null");
+        }
+
 
         return root;
     }
+
+    /**
+     * Check if this device has a camera
+     */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    /**
+     * A safe way to get an instance of the Camera object.
+     */
+    public static Camera getCameraInstance() {
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        } catch (Exception e) {
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+
 }
--- app/src/main/java/com/example/artia/ui/search/SearchViewModel.java
+++ app/src/main/java/com/example/artia/ui/search/SearchViewModel.java
@@ -6,14 +6,13 @@ import androidx.lifecycle.ViewModel;
 
 public class SearchViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    private MutableLiveData<CameraPreview> cameraPreview;
 
     public SearchViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is search fragment");
+        cameraPreview = new MutableLiveData<>();
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<CameraPreview> getCameraPreview() {
+        return cameraPreview;
     }
 }
