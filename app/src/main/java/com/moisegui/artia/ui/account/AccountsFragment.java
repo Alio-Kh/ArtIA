122d2e46b07d0e26d3c679f551289365f6245865
Moïse Gui <k.moise.gui@gmail.com>
2021-02-19 02:06:33 +0100

    Login process implemented

--- app/src/main/java/com/moisegui/artia/ui/account/AccountsFragment.java
+++ app/src/main/java/com/moisegui/artia/ui/account/AccountsFragment.java
@@ -9,15 +9,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.moisegui.artia.R;
@@ -30,29 +32,48 @@ public class AccountsFragment extends Fragment {
     private AccountsViewModel accountsViewModel;
     FirebaseAuth auth;
 
+    Button btnConnxion;
+    TextView textView;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         accountsViewModel =
                 new ViewModelProvider(this).get(AccountsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_account, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        accountsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        textView = root.findViewById(R.id.text_notifications);
+//        accountsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
 
-        Button btnConnxion = root.findViewById(R.id.btnConnexion);
-
-
+        btnConnxion = root.findViewById(R.id.btnConnexion);
         auth = FirebaseAuth.getInstance();
+
+        refreshInterface();
+
+        return root;
+    }
+
+    public void refreshInterface() {
         if (auth.getCurrentUser() != null) {
             // already signed in
-            btnConnxion.setVisibility(View.INVISIBLE);
-            textView.setText("You are connected");
+            textView.setText("You are Logged in");
+            btnConnxion.setText(R.string.log_out);
+
+            btnConnxion.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    logOut();
+                }
+            });
+
         } else {
             // not signed in
+            textView.setText("You are not Logged in");
+            btnConnxion.setText(R.string.log_in);
+
             btnConnxion.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -63,8 +84,6 @@ public class AccountsFragment extends Fragment {
                 }
             });
         }
-
-        return root;
     }
 
     @Override
@@ -78,14 +97,42 @@ public class AccountsFragment extends Fragment {
                 // Successfully signed in
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 Log.v(TAG, "Logged in");
+                Toast.makeText(getContext(), String.valueOf(R.string.logged_in), Toast.LENGTH_SHORT).show();
                 // ...
+                refreshInterface();
             } else {
-                Log.v(TAG, "Logged in Failed");
+                Log.v(TAG, String.valueOf(R.string.login_failed));
+                Toast.makeText(getContext(), String.valueOf(R.string.login_failed), Toast.LENGTH_SHORT).show();
                 // Sign in failed. If response is null the user canceled the
                 // sign-in flow using the back button. Otherwise check
                 // response.getError().getErrorCode() and handle the error.
                 // ...
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    Toast.makeText(getContext(), String.valueOf(R.string.log_in_cancelled), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    Toast.makeText(getContext(), String.valueOf(R.string.no_internet_connection), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Toast.makeText(getContext(), String.valueOf(R.string.unknown_error), Toast.LENGTH_SHORT).show();
+                Log.e(TAG, "Sign-in error: ", response.getError());
             }
         }
+    }
+
+    public void logOut() {
+        AuthUI.getInstance()
+                .signOut(getContext())
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // user is now signed out
+                        refreshInterface();
+                    }
+                });
     }
 }
